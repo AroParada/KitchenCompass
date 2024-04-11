dea5f79c60c45c6b740f813da3e84b0464b53e2e
Aro Parada <haroldparada20@gmail.com>
2024-04-11 00:52:26 -0400

    added fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,6 @@
 import icons from 'url:../../img/icons.svg';
+import {Fraction} from 'fractional'
+console.log(Fraction);
 
 class recipeView {
   #parentElement = document.querySelector('.recipe');
@@ -92,7 +94,7 @@ class recipeView {
               <svg class="recipe__icon">
                 <use href="src/img/icons.svg#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
+              <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
